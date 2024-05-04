815a5b9117dac37c04d49b49a7cbb5a79898ebcc
Harsh <harshsheth9909@gmail.com>
2024-05-04 13:51:46 +0530

    added loding and removed unused code

--- src/App.js
+++ src/App.js
@@ -1,40 +1,26 @@
 import './App.css';
-import { useState, useEffect,useCallback } from "react";
+import { useState, useEffect } from "react";
 import JobCard from './components/JobCard';
-import { FormControl, Grid, InputLabel, MenuItem, Select, TextField } from '@mui/material';
+import { CircularProgress, FormControl, Grid, InputLabel, MenuItem, Select, TextField } from '@mui/material';
 
 function App() {
-  const [jobs, setJobs] = useState([]); // State for fetching data from api
-  const [offset, setOffset] = useState(0);  // State of fetching data from api for next request
-  const [records, setRecords] = useState([]); // State for filtering data from api
-  const [selectedLocation, setSelectedLocation] = useState(""); // State for selected location
-  const [selectedRole, setSelectedRole] = useState(""); // State for selected role
-  const [locations, setLocations] = useState([]); // State for locations
-  const [roles, setRoles] = useState([]); // State for roles
-  const [noDataMessage, setNoDataMessage] = useState(""); // State for no data/jobs message
-  const [searchQuery, setSearchQuery] = useState(""); // State for company name search
-  const [expRange, setExpRange] = useState(""); // State for experience range
-  const [salaryRange, setSalaryRange] = useState(""); // State for salary range
+  const [jobs, setJobs] = useState([]); 
+  const [offset, setOffset] = useState(0);  
+  const [records, setRecords] = useState([]); 
+  const [selectedLocation, setSelectedLocation] = useState(""); 
+  const [selectedRole, setSelectedRole] = useState(""); 
+  const [locations, setLocations] = useState([]); 
+  const [roles, setRoles] = useState([]); 
+  const [noDataMessage, setNoDataMessage] = useState("");
+  const [searchQuery, setSearchQuery] = useState(""); 
   const [selectedExperienceRange, setSelectedExperienceRange] = useState('');
   const [selectedSalaryRange, setSelectedSalaryRange] = useState('');
+  const [isLoading,setIsLoading]=useState(false)
 
-  const expRanges = [
-    { label: "1-3 years", min: 1, max: 3 },
-    { label: "3-6 years", min: 3, max: 6 },
-    { label: "6-10 years", min: 6, max: 10 },
-    { label: "10+ years", min: 10, max: Infinity }
-  ];
-
-  const salaryRanges = [
-    { label: "1-10 USD", min: 1, max: 10 },
-    { label: "10-20 USD", min: 10, max: 20 },
-    { label: "20-50 USD", min: 20, max: 50 },
-    { label: "50-100 USD", min: 50, max: 100 },
-    { label: "100+ USD", min: 100, max: Infinity }
-  ];
 
   const fetchData = async () => {
     try {
+      setIsLoading(true)
       const myHeaders = new Headers();
       myHeaders.append("Content-Type", "application/json");
 
@@ -69,12 +55,14 @@ function App() {
 
     } catch (error) {
       console.error("Error fetching job listings:", error);
+    } finally{
+      setIsLoading(false)
     }
   };
 
   // get more data after scroll hit the bottom
   const handleScroll = async () => {
-    if (window.innerHeight + document.documentElement.scrollTop >= document.documentElement.offsetHeight) {
+    if (window.innerHeight + document.documentElement.scrollTop >= document.documentElement.offsetHeight-30) {
       fetchData();
     }
   };
@@ -142,79 +130,85 @@ console.log(selectedLocation)
 
   return (
     <div className="App">
-    <Grid container spacing={2} justifyContent="center">
-      <Grid item xs={12} md={10} lg={8}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={6} md={4} lg={3}>
-            <FormControl variant="filled" fullWidth>
-              <InputLabel>Roles</InputLabel>
-              <Select value={selectedRole} onChange={handleRoleChange}>
-                <MenuItem value="">All Roles</MenuItem>
-                {roles.map((role, index) => (
-                  <MenuItem key={index} value={role}>{role}</MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4} lg={3}>
-            <FormControl variant="filled" fullWidth>
-              <InputLabel>Locations</InputLabel>
-              <Select value={selectedLocation} onChange={handleLocationChange}>
-                <MenuItem value="">All Locations</MenuItem>
-                {locations.map((location, index) => (
-                  <MenuItem key={index} value={location}>{location}</MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4} lg={3}>
-            <FormControl variant="filled" fullWidth>
-              <InputLabel>Experience</InputLabel>
-              <Select value={selectedExperienceRange ? selectedExperienceRange.join(',') : ""} onChange={handleExperienceRangeChange}>
-                <MenuItem value="1,3">1-3 years</MenuItem>
-                <MenuItem value="3,6">3-6 years</MenuItem>
-                <MenuItem value="6,10">6-10 years</MenuItem>
-                <MenuItem value="10,Infinity">10+ years</MenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4} lg={3}>
-            <FormControl variant="filled" fullWidth>
-              <InputLabel>Salary</InputLabel>
-              <Select value={selectedSalaryRange ? selectedSalaryRange.join(',') : ''} onChange={handleSalaryRangeChange}>
-                <MenuItem value="1,10">1-10 USD</MenuItem>
-                <MenuItem value="10,20">10-20 USD</MenuItem>
-                <MenuItem value="20,50">20-50 USD</MenuItem>
-                <MenuItem value="50,100">50-100 USD</MenuItem>
-                <MenuItem value="100,Infinity">100+ USD</MenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={12}>
-            <TextField 
-              label="Company Name" 
-              variant="outlined" 
-              fullWidth
-              value={searchQuery} 
-              onChange={handleSearchChange} 
-            />
+    <Grid container spacing={2} p={1} justifyContent="center">
+        <Grid item xs={12} md={10} lg={8}>
+          <Grid container spacing={2} >
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+              <FormControl size="small" variant="filled" fullWidth>
+                <InputLabel>Roles</InputLabel>
+                <Select value={selectedRole} onChange={handleRoleChange}>
+                  <MenuItem value="">All Roles</MenuItem>
+                  {roles.map((role, index) => (
+                    <MenuItem key={index} value={role}>{role}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+              <FormControl size="small" variant="filled" fullWidth>
+                <InputLabel>Locations</InputLabel>
+                <Select value={selectedLocation} onChange={handleLocationChange}>
+                  <MenuItem value="">All Locations</MenuItem>
+                  {locations.map((location, index) => (
+                    <MenuItem key={index} value={location}>{location}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+              <FormControl size="small" variant="filled" fullWidth>
+                <InputLabel>Experience</InputLabel>
+                <Select value={selectedExperienceRange ? selectedExperienceRange.join(',') : ""} onChange={handleExperienceRangeChange}>
+                  <MenuItem value="1,3">1-3 years</MenuItem>
+                  <MenuItem value="3,6">3-6 years</MenuItem>
+                  <MenuItem value="6,10">6-10 years</MenuItem>
+                  <MenuItem value="10,Infinity">10+ years</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+              <FormControl size="small" variant="filled" fullWidth>
+                <InputLabel>Salary</InputLabel>
+                <Select value={selectedSalaryRange ? selectedSalaryRange.join(',') : ''} onChange={handleSalaryRangeChange}>
+                  <MenuItem value="1,10">1-10 USD</MenuItem>
+                  <MenuItem value="10,20">10-20 USD</MenuItem>
+                  <MenuItem value="20,50">20-50 USD</MenuItem>
+                  <MenuItem value="50,100">50-100 USD</MenuItem>
+                  <MenuItem value="100,Infinity">100+ USD</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+              size="small"
+                label="Company Name"
+                variant="outlined"
+                fullWidth
+                value={searchQuery}
+                onChange={handleSearchChange}
+              />
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
-    </Grid>
-    <Grid container spacing={2} justifyContent="center" p={3}>
-      {records.length > 0 ? (
-        records.map((job, index) => (
-          <Grid key={index} item xs={12} sm={6} md={4} lg={3}>
-            <JobCard job={job} />
+      <Grid container spacing={2} justifyContent="center" p={3}>
+        {records.length > 0 ? (
+          records.map((job, index) => (
+            <Grid key={index} item xs={12} sm={6} md={4} lg={3}>
+              <JobCard job={job} />
+            </Grid>
+          ))
+        ) : (
+          <Grid item xs={12}>
+            {noDataMessage && <p>{noDataMessage}</p>}
           </Grid>
-        ))
-      ) : (
-        <Grid item xs={12}>
-          {noDataMessage && <p>{noDataMessage}</p>}
-        </Grid>
+        )}
+      </Grid>
+      {isLoading && (
+        <div style={{ position: 'fixed', bottom: 20, left: '50%', transform: 'translateX(-50%)', zIndex: 999, display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(255, 255, 255, 0.8)', padding: 20, borderRadius: 10 }}>
+          <CircularProgress  />
+        </div>
       )}
-    </Grid>
   </div>
   );
 }
--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Card, CardContent, Dialog, DialogActions, DialogContent, DialogTitle, Icon, Typography } from '@mui/material';
-import { AttachMoneyOutlined, Info } from '@mui/icons-material';
+import { Button, Card, CardContent, Dialog, DialogActions, DialogContent, DialogTitle, Typography } from '@mui/material';
 
 const JobCard = ({ job }) => {
   
@@ -8,8 +7,8 @@ const JobCard = ({ job }) => {
 
   const [expanded, setExpanded] = useState(false);
 
-  const shortDescription = jobDetailsFromCompany.length > 200
-    ? `${jobDetailsFromCompany.substring(0, 200)}...`
+  const shortDescription = jobDetailsFromCompany.length > 250
+    ? `${jobDetailsFromCompany.substring(0, 250)}...`
     : jobDetailsFromCompany;
 
   const handleExpand = () => {
@@ -17,9 +16,40 @@ const JobCard = ({ job }) => {
   };
 
   return (
-    <Card sx={{ borderRadius: 8 }}>
+    <Card sx={{ borderRadius: 8 }} elevation={3}>
       <CardContent style={{ display: 'flex', flexDirection: 'column',alignItems: 'flex-start' }}>
         <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
+          <img src={logoUrl} alt={companyName} style={{ height: 80, width: 80, marginRight: 30 }} />
+          <div style={{ display: 'flex', flexDirection: 'column',alignItems: 'flex-start' }}>
+            <Typography variant="h5">{jobRole}</Typography>
+            <Typography variant="subtitle1">{companyName}</Typography>
+            <Typography variant="subtitle2">{location}</Typography>
+          </div>
+        </div>
+        {(minJdSalary && salaryCurrencyCode)?( <Typography variant="subtitle1">Estimated Salary: {minJdSalary} {salaryCurrencyCode} ✅</Typography>):( <Typography variant="subtitle1">Estimated Salary: Not mentioned</Typography>)}
+       
+        <Typography variant="h6" style={{ marginTop: 10 }}>About Company</Typography>
+        <Typography variant="body2" style={{ textAlign: 'left' }}>{shortDescription}</Typography>
+        {jobDetailsFromCompany.length > 200 && (
+          <Button style={{ alignSelf: 'center', marginTop:2, marginBottom:2 }} variant="text" color="primary" onClick={handleExpand}>View Job</Button>
+        )}
+        {(minExp && maxExp) ?( <Typography variant="subtitle1">Experience: {minExp}-{maxExp} years</Typography>):(<Typography variant="subtitle1">Experience: Not mentioned</Typography>)}
+       
+        <div style={{ marginTop: 10, width:"100%", }}>
+          <Button fullWidth variant="contained" href={jdLink} target="_blank"  style={{ backgroundColor:"#5CE7BD",color:"black"  }}>⚡Easy Apply</Button>
+          
+        </div>
+       
+        <div style={{ marginTop: 10, width:"100%" }}>
+         
+          <Button  style={{ backgroundColor:"#4C46DA",color:"white"}} href={jdLink} target="_blank"  fullWidth variant="contained" color="secondary">🔓 Unlock Referrals</Button>
+        </div>
+      </CardContent>
+      {expanded && (
+        <Dialog open={expanded} onClose={() => setExpanded(false)}>
+          <DialogTitle>{jobRole} - {companyName}</DialogTitle>
+          <DialogContent>
+             <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
           <img src={logoUrl} alt={companyName} style={{ height: 80, width: 80, marginRight: 30 }} />
           <div style={{ display: 'flex', flexDirection: 'column',alignItems: 'flex-start' }}>
             <Typography variant="h5">{jobRole}</Typography>
@@ -27,33 +57,26 @@ const JobCard = ({ job }) => {
             <Typography variant="body2">{location}</Typography>
           </div>
         </div>
-        <Typography variant="body2">Estimated Salary: {minJdSalary} {salaryCurrencyCode} ✅</Typography>
+        {(minJdSalary && salaryCurrencyCode)?( <Typography variant="body2">Estimated Salary: {minJdSalary} {salaryCurrencyCode} ✅</Typography>):( <Typography variant="body2">Estimated Salary: Not mentioned</Typography>)}
+       
         <Typography variant="h6" style={{ marginTop: 10 }}>About Company</Typography>
-        <Typography variant="body2" style={{ textAlign: 'left' }}>{shortDescription}</Typography>
-        {jobDetailsFromCompany.length > 200 && (
-          <Button style={{ alignSelf: 'center', marginTop:2, marginBottom:2 }} variant="text" color="primary" onClick={handleExpand}>View Job</Button>
-        )}
+        <Typography variant="body2" style={{ textAlign: 'left' }}>{jobDetailsFromCompany}</Typography>
+        
         {(minExp && maxExp) ?( <Typography variant="body2">Experience: {minExp}-{maxExp} years</Typography>):(<Typography variant="body2">Experience: Not mentioned</Typography>)}
        
         <div style={{ marginTop: 10, width:"100%", }}>
-          <Button fullWidth variant="contained"  style={{ backgroundColor:"#5CE7BD",color:"black"  }}>⚡Easy Apply</Button>
+          <Button fullWidth variant="contained" href={jdLink} target="_blank"   style={{ backgroundColor:"#5CE7BD",color:"black"  }}>⚡Easy Apply</Button>
           
         </div>
        
         <div style={{ marginTop: 10, width:"100%" }}>
          
-          <Button  style={{ backgroundColor:"#4C46DA",color:"white"}} fullWidth variant="contained" color="secondary">🔓 Unlock Referrals</Button>
+          <Button href={jdLink} target="_blank"  style={{ backgroundColor:"#4C46DA",color:"white"}} fullWidth variant="contained" color="secondary">🔓 Unlock Referrals</Button>
         </div>
-      </CardContent>
-      {expanded && (
-        <Dialog open={expanded} onClose={() => setExpanded(false)}>
-          <DialogTitle>{jobRole} - {companyName}</DialogTitle>
-          <DialogContent>
-            <Typography>{jobDetailsFromCompany}</Typography>
           </DialogContent>
           <DialogActions>
             <Button onClick={() => setExpanded(false)} color="primary">Close</Button>
-            <Button href={jdLink} target="_blank" rel="noopener noreferrer" color="primary">Apply Now</Button>
+           
           </DialogActions>
         </Dialog>
       )}
